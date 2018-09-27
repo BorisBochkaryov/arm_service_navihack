86ead4f7364143dfb4af0f3ea06c7e139e35a0de
Boris Bochkarev <Boris-Bochkaryov@yandex.ru>
2018-09-27 09:16:37 +0700

    Получение категорий и подкатегорий

--- Server/DB.py
+++ Server/DB.py
@@ -1,15 +1,42 @@
 import sqlite3
 from flask import jsonify
 
-def getUserData(id, json=False):
+def __sql_exec_one(sql):
     conn = sqlite3.connect("db.db")
     cursor = conn.cursor()
-    sql = "SELECT * FROM users WHERE id = '" + str(id) + "';"
     cursor.execute(sql)
-    result_user = cursor.fetchone()
-    id, fio, address = result_user
+    result = cursor.fetchone()
     conn.close()
+    return result
+
+def __sql_exec_all(sql):
+    conn = sqlite3.connect("db.db")
+    cursor = conn.cursor()
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    conn.close()
+    return result
+
+def getUserData(id, json=False):
+    result_user = __sql_exec_one("SELECT * FROM users WHERE id = '" + str(id) + "'")
+    id, fio, address, phone = result_user
     if json == True:
-        return jsonify({'id':id, 'fio':fio, 'address':address})
+        return jsonify({'id':id, 'fio':fio, 'address':address, 'phone':phone})
     else:
         return result_user
+
+def getCategory(json=False):
+    result_category = __sql_exec_all("SELECT * FROM category")
+    categories = [dict(zip(('id', 'title'), X)) for X in result_category]
+    if json == True:
+        return jsonify({'status':'ok', 'categories':categories})
+    else:
+        return result_category
+
+def getSubCategory(id, json=False):
+    result_subcategory = __sql_exec_all("SELECT * FROM subcategory WHERE category_id = '" + str(id) + "'")
+    subcategories = [dict(zip(('id', 'category_id', 'title'), X)) for X in result_subcategory]
+    if json == True:
+        return jsonify({'status':'ok', 'category_id':id, 'subcategories':subcategories})
+    else:
+        return result_subcategory
--- Server/main.py
+++ Server/main.py
@@ -11,6 +11,14 @@ def index():
 def getUserData(id):
     return DB.getUserData(id, json=True)
 
+@app.route("/get_category")
+def getCategory():
+    return DB.getCategory(json=True)
+
+@app.route("/get_subcategory/<id>")
+def getSubCategory(id):
+    return DB.getSubCategory(id, json=True)
+
 if __name__ == "__main__":
     DB.getUserData(1)
     app.run()
