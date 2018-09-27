538b772d39ce9f46babab0c7850222f7011d2706
Boris Bochkarev <Boris-Bochkaryov@yandex.ru>
2018-09-27 13:08:23 +0700

    Изменение данных пользователя, получение списка работников, добавление заявки

--- Server/DB.py
+++ Server/DB.py
@@ -17,6 +17,14 @@ def __sql_exec_all(sql):
     conn.close()
     return result
 
+def __sql_exec(sql):
+    conn = sqlite3.connect("db.db")
+    cursor = conn.cursor()
+    cursor.execute(sql)
+    conn.commit()
+    conn.close()
+
+
 def getUserData(id, json=False):
     result_user = __sql_exec_one("SELECT * FROM users WHERE id = '" + str(id) + "'")
     id, fio, address, phone = result_user
@@ -24,6 +32,9 @@ def getUserData(id, json=False):
         return jsonify({'id':id, 'fio':fio, 'address':address, 'phone':phone})
     else:
         return result_user
+
+def setUserData(id, field, data):
+    __sql_exec("UPDATE users SET " + str(field) + " = '" + str(data) + "' WHERE id = '" + str(id) + "'")
 
 def getCategory(json=False):
     result_category = __sql_exec_all("SELECT * FROM category")
@@ -40,3 +51,15 @@ def getSubCategory(id, json=False):
         return jsonify({'status':'ok', 'category_id':id, 'subcategories':subcategories})
     else:
         return result_subcategory
+
+def getStaff(category, subcategory, json=False):
+    result_staff = __sql_exec_all("SELECT * FROM staff WHERE category = '" + str(category) + "' AND subcategory = '" + str(subcategory) + "'")
+    staff = [dict(zip(('id','category','subcategory','fio','price','phone'), X)) for X in result_staff]
+    if json == True:
+        return  jsonify({'status':'ok', 'staff':staff})
+    else:
+        return result_staff
+
+def addOrder(user_id, staff_id, date, comment):
+    sql = "INSERT INTO orders(user_id, staff_id, date, comment) VALUES ('" + str(user_id) + "', '" + str(staff_id) + "', '" + str(date) + "', '" + str(comment) + "');"
+    __sql_exec(sql)
--- Server/main.py
+++ Server/main.py
@@ -11,6 +11,11 @@ def index():
 def getUserData(id):
     return DB.getUserData(id, json=True)
 
+@app.route("/set_user_data/<id>/<field>/<data>")
+def setUserData(id, field, data):
+    DB.setUserData(id, field, data)
+    return jsonify({'status':'ok'})
+
 @app.route("/get_category")
 def getCategory():
     return DB.getCategory(json=True)
@@ -19,6 +24,14 @@ def getCategory():
 def getSubCategory(id):
     return DB.getSubCategory(id, json=True)
 
+@app.route("/get_staff/<category>/<subcategory>")
+def getStaff(category, subcategory):
+    return DB.getStaff(category, subcategory, json=True)
+
+@app.route("/add_order/<user_id>/<staff_id>/<date>/<comment>")
+def addOrder(user_id, staff_id, date, comment):
+    DB.addOrder(user_id, staff_id, date, comment)
+    return jsonify({'status':'ok'})
+
 if __name__ == "__main__":
-    DB.getUserData(1)
     app.run()
